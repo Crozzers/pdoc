6f01c475dce5eaf15d8909cd9c44bb2692bb42d1
Crozzers <captaincrozzers@gmail.com>
2023-05-14 12:43:32 +0100

    Modify docstring to showcase potential bug

--- test/testdata/demopackage/child_c.py
+++ test/testdata/demopackage/child_c.py
@@ -3,7 +3,10 @@ from . import child_b
 
 
 class C(child_b.B):
-    """This class is defined in .child_c and inherits from .child_b.B"""
+    """This class is defined in .child_c and inherits from .child_b.B
+
+    Here's an identifier that isn't enclosed in back ticks: demopackage.child_b.B
+    """
 
     def c(self):
         return 2
